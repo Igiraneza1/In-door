506450b3b91f9a34bcdbaeb2939cc2b8ddb79cb1
Igiraneza1 <igiranezaadeline@gmail.com>
2025-07-14 13:58:51 +0300

    homepage

---
+++ src/app/inDoor/page.tsx
@@ -0,0 +1,9 @@
+import HomePage from "./home/page";
+
+export default function Indoor() {
+    return (
+        <div className="max-w-5xl bg-white mx-auto p-10">
+            <HomePage />
+        </div>
+    )
+}
