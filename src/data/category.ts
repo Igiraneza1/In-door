de35d38231d3b38593f3758dabb42a7f12a8c6b8
Igiraneza1 <igiranezaadeline@gmail.com>
2025-07-17 16:41:16 +0300

    commit

--- src/data/category.ts
+++ src/data/category.ts
@@ -1,39 +1,39 @@
+
 const categories = [
   {
     id: 1,
     name: "Living Room",
     slug: "living-room",
     image: "/image/Living-room/living room category.jpg",
-    link: "/src/app/categories",
+    link: "/categories/living-room",
   },
   {
     id: 2,
     name: "Bedroom",
     slug: "bedroom",
     image: "/image/bedroom/bedroom10.jpg",
-    link: "/bedroom",
+    link: "/categories/bedroom", 
   },
   {
     id: 3,
     name: "Kitchen",
     slug: "kitchen",
     image: "/image/kitchen/kitchen5.jpg",
-    link: "/kitchen",
+    link: "/categories/kitchen",  
   },
   {
     id: 4,
     name: "Bathroom",
     slug: "bathroom",
     image: "/image/bathroom/bathroom4.jpg",
-    link: "/categories/bathroom",
+    link: "/categories/Bathroom",  
   },
   {
     id: 5,
     name: "Dinning",
     slug: "dinning",
     image: "/image/Dining/dining1.jpg",
-    link: "/dinning",
+    link: "/categories/dinning", 
   },
 ];
-
 export default categories;
