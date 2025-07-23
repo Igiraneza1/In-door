3046aab507a9827b7806b8e6841644b83d53338c
Umubyeyi-Marie <mariebienvenueu@gmail.com>
2025-07-23 18:32:25 +0200

    updated cart

--- src/app/navbar/page.tsx
+++ src/app/navbar/page.tsx
@@ -13,7 +13,7 @@ interface NavLink {
 
 const navLinks: NavLink[] = [
   { name: "Home", href: "/inDoor" },
-  { name: "Shop", href: "/shop" },
+  { name: "Product", href: "/product" },
   { name: "Contact", href: "/contact" },
   // { name: "Dashboard", href: "/user-dashboard" },
 ];
